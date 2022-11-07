3a9a322b8e21212cbc2d7597a3eddc647280fdce
fhd <rohithvenkatesan142@gmail.com>
2022-11-07 00:39:40 -0500

    revert

--- components/Landing.js
+++ components/Landing.js
@@ -11,7 +11,7 @@ const Landing = () => {
       {<Image className='fixed invisible md:visible' src="/assets/npng.jpg" layout='fill' quality='100' />}
       {<Image className='fixed visible md:invisible' src="/assets/jj.png" layout='fill' quality='100' />}
       <h1 className='absolute top-40 p-4 text-6xl animate-type'> Mai Finance Vault Analytics</h1>
-      <button onClick = {() => router.push('/vaults')} className='border-4 border-black rounded-lg p-4 bg-[#EDE6D4] font-bold absolute top-60 text-4xl mt-40 hover:shadow-xl hover:text-[#EDE6D4] hover:bg-black hover:border-[#EDE6D4]'> Enter App</button>
+      <button onClick={() => router.push('/vaults')} className='border-4 border-black rounded-lg p-4 bg-[#EDE6D4] font-bold absolute top-60 text-4xl mt-40 hover:shadow-xl hover:text-[#EDE6D4] hover:bg-black hover:border-[#EDE6D4]'> Enter App</button>
       <ArrowCircleDownIcon className='hidden absolute h-12 w-12 bottom-0 animate-pulse animate-bounce' />
     </div>
   )
